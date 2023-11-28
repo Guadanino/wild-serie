1a82436e92b9dcf4e3cbc0d173c61a9fb96f3188
Romain GUIOT <romain.guiot@hotmail.fr>
2023-11-28 15:21:42 +0100

    quête 7

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    const CATEGORY = ['Action', 'Avanture', 'Animation', 'Fantastique', 'Horreur',];
+
+    public function load(ObjectManager $manager): void
+    {
+        foreach (self::CATEGORY as $key => $categoryName) {
+            $category = new Category();
+            $category->setName($categoryName);
+            $manager->persist($category);
+        }
+        $manager->flush();
+    }
+}
