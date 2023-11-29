4fe72ae48809eebe1ff0423b0cab7c8d6bc37bec
Romain GUIOT <romain.guiot@hotmail.fr>
2023-11-29 09:46:44 +0100

    quete 9

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -8,14 +8,18 @@ use Doctrine\Persistence\ObjectManager;
 
 class CategoryFixtures extends Fixture
 {
-    const CATEGORY = ['Action', 'Avanture', 'Animation', 'Fantastique', 'Horreur',];
+    const CATEGORY = [
+        'Action', 'Aventure', 'Animation', 'Fantastique', 'Horreur',
+    ];
 
     public function load(ObjectManager $manager): void
     {
-        foreach (self::CATEGORY as $key => $categoryName) {
+        foreach (self::CATEGORY as $categoryName) {
             $category = new Category();
             $category->setName($categoryName);
             $manager->persist($category);
+
+            $this->addReference('category_' . $categoryName, $category);
         }
         $manager->flush();
     }
---
+++ src/DataFixtures/ProgramFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Program;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class ProgramFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $program = new Program();
+        $program->setTitle('Walking dead', 'Narnia et l\'armoir magique', 'Indiana Jones', 'Aladdin', 'L\exorciste');
+        $program->setSynopsis('Des zombies envahissent la terre', 'magie', 'Le temple maudit', 'Le roi des voleurs', 'Bouhhhhh');
+        $program->setCategory($this->getReference('category_Fantastique'));
+        $manager->persist($program);
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+          CategoryFixtures::class,
+        ];
+    }
+
+
+}
